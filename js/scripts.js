116f86dcbd75056290a683d26ef13e55d048f5a1
Peter Kingori <kpndegwa@gmail.com>
2019-07-14 20:29:48 +0300

    Add a function in the JS file that will get the user's input of date of birth including day of the month, month and year. The function will also use the parseInt() function to change the inputs from strings to numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 function buttonClicked(id) {
+	// Will display either the form or a message to the user depending on whether he/she selects Yes or No.
 	if (id === 'yesButton') {
 		document.getElementById('form').style.display = 'block';
 		document.getElementById('noClick').style.display = 'none';
@@ -6,4 +7,15 @@ function buttonClicked(id) {
 		document.getElementById('form').style.display = 'none';
 		document.getElementById('noClick').style.display = 'block';
 	}
+}
+
+function checkValid() {
+	// Get the values of the day and month from the form
+	var day = document.getElementById('userDay').value;
+  var dayInt = parseInt(day);
+  var month = document.getElementById("userMonth").value;
+  var monthInt = parseInt(month);
+  var yearValue = document.getElementById("userYear").value;
+  var yearInteger = parseInt(yearValue);
+	console.log(day, month, yearValue);
 }
